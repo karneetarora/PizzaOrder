3fb3cd4fc6c01f7ef1719d401df1d68aff117cf9
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-04-06 16:21:34 -0400

    Add files via upload

---
+++ p4controller.java
@@ -0,0 +1,58 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
+
+public class p4controller {
+	
+    @FXML
+    private Label pizza;
+    
+    @FXML
+    private Label size;
+
+    @FXML
+    private MenuButton build;
+    
+    @FXML
+    private MenuButton selectSize;
+
+    @FXML
+    private Button addToOrder;
+
+    @FXML
+    private Button show;
+    
+    @FXML
+    private Button addT;
+    
+    @FXML
+    private Button removeT;
+    
+    @FXML
+    private Button clearT;
+
+    @FXML
+    private TextArea textArea;
+    
+    @FXML
+    public void addT(ActionEvent event) {
+    	
+    }
+    
+    @FXML
+    public void removeT(ActionEvent event) {
+    	
+    }
+    
+    @FXML
+    public void clearT(ActionEvent event) {
+    	
+    }
+}
