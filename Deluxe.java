c6e7718bc3652767777c9172cf3f303aaf8b74ff
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-04-09 16:59:34 -0400

    Update Deluxe.java

--- Deluxe.java
+++ Deluxe.java
@@ -1,11 +1,48 @@
-package application;
+ /**
+ * Deluxe Class
+ * Methods include - pizzaPrice(), toString()
+ * 
+ * @author Karneet Arora, Manel Bermad
+ */
 
-public class Deluxe extends Pizza{
+package application;
+import java.util.ArrayList; 
+
+public class Deluxe extends Pizza {
+
+	final static int small = 14; 
+	final static int surchargeMed = 2; 
+	final static int surchargeLarge = 4; 
+	public static String style; 
+	public static String size;
+	ArrayList<String> top = new List<String>();
+	
+	
+	public Deluxe(String style, String size,  ArrayList<String> selectedToppingsList) {
+		super(style, size, selectedToppingsList);
+	}
 
 	@Override
 	public int pizzaPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		switch(size) {
+		case "Small":
+			return small;
+		case "Medium":
+			return small + surchargeMed; 
+		case "Large":
+			return small + surchargeMed + surchargeLarge; 
+		default:
+			return small;
+		}
+	}
+	
+	@Override
+	/**
+	* toString print the style, size, and toppings of the pizza 
+	* @return pizza details as a String
+	*/
+	public String toString() {
+		return style + " " + size + " " + top;
 	}
 
 }
