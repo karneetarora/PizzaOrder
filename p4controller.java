35024867775befa61cf6c6b30c6cd157d38a800d
manelbermad <42884143+manelbermad@users.noreply.github.com>
2020-04-09 02:08:01 -0400

    Update p4controller.java

--- p4controller.java
+++ p4controller.java
@@ -1,38 +1,17 @@
 package application;
 
+import java.lang.String;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class p4controller {
-
-    @FXML
-    private MenuButton buildMenu;
-
-    @FXML
-    private MenuItem pizzaDeluxe;
-
-    @FXML
-    private MenuItem pizzaHawaiian;
-
-    @FXML
-    private MenuItem pizzaBuildYourOwn;
-
-    @FXML
-    private MenuButton sizeMenu;
-
-    @FXML
-    private MenuItem sizeSmall;
-
-    @FXML
-    private MenuItem sizeMedium;
-
-    @FXML
-    private MenuItem sizeLarge;
 
     @FXML
     private Button buttonAddTopping;
@@ -59,91 +38,110 @@ public class p4controller {
     private ImageView imageView;
 
     @FXML
-    void actionAddToOrder(ActionEvent event) {
+    private ComboBox<String> pizzaComboBox;
 
-    }
+
+    @FXML
+    private ComboBox<String> sizeComboBox;
+
+    @FXML
+    private TextArea textArea;
+    
+    int toppingsCounter = 0;
 
     @FXML
     void actionAddTopping(ActionEvent event) {
+    	// check counter!
+    	if(toppingsCounter < 6  && !toppingsList.getSelectionModel().isEmpty()) { // and has selected something
+    		toppingsCounter++;
+    		selectedToppingsList.getItems().add(toppingsList.getSelectionModel().getSelectedItem());
+    		toppingsList.getItems().remove(toppingsList.getSelectionModel().getSelectedItem());
+    		    		
+    	}
 
     }
-
-    @FXML
-    void actionClearSelection(ActionEvent event) {
-    	selectedToppingsList.getItems().clear();
-
-    }
-
-    @FXML
+    
+   @FXML
     void actionRemoveTopping(ActionEvent event) {
 
+	   if(!selectedToppingsList.getSelectionModel().isEmpty()) {
+		   toppingsList.getItems().add(selectedToppingsList.getSelectionModel().getSelectedItem());
+		   selectedToppingsList.getItems().remove(selectedToppingsList.getSelectionModel().getSelectedItem());
+	   }
+    }
+   
+    @FXML
+    void actionClearSelection(ActionEvent event) {
+    	toppingsList.getItems().clear();
+    	pizzaComboBox.setValue("Build Your Own");
+
     }
 
+    Image photoBuildYourOwnPizza  =  new Image("application/BuildYourOwnPizza.png");
+    Image photoHawaiianPizza = new Image("application/HawaiianPizza.png");
+    Image photoDeluxPizza = new Image("application/DeluxPizza.png");
+
+    @FXML
+    void selectedPizza(ActionEvent event) {
+    	toppingsCounter = 0;
+    	if(pizzaComboBox.getValue().equals("Hawaiian")) {
+    		
+    		imageView.setImage(photoHawaiianPizza);
+   		
+        	toppingsList.getItems().clear();
+        	toppingsList.setDisable(true);
+        	buttonAddTopping.setDisable(true);
+        	buttionRemoveTopping.setDisable(true);
+        	selectedToppingsList.getItems().clear();
+        	selectedToppingsList.getItems().addAll("Ham", "Pineapple");
+        	
+    	}else if(pizzaComboBox.getValue().equals("Deluxe")) {
+    		imageView.setImage(photoDeluxPizza);
+
+        	toppingsList.getItems().clear();
+        	toppingsList.setDisable(true);
+        	buttonAddTopping.setDisable(true);
+        	buttionRemoveTopping.setDisable(true);
+        	selectedToppingsList.getItems().clear();
+        	selectedToppingsList.getItems().addAll("Sausage", "Pepperoni", "Green Pepper", 
+        											"Onion", "Mushroom");// collection?
+        	
+    	}else { // if build your own
+    		imageView.setImage(photoBuildYourOwnPizza);
+
+        	toppingsList.getItems().clear();
+        	toppingsList.setDisable(false);
+        	buttonAddTopping.setDisable(false);
+        	selectedToppingsList.getItems().clear();
+
+        	toppingsList.getItems().addAll("Chicken", "Ham", "Mushroom", "Onion", "Pepperoni",
+        										"Pineapple", "Saussage","Beef", "Cheese", "Green Pepper");
+
+    	}
+
+    }
+    
+    @FXML
+    void actionAddToOrder(ActionEvent event) {
+    	textArea.clear();
+    	if (!selectedToppingsList.getItems().isEmpty()) {
+    		textArea.appendText("Your pizza has been added to your order.");
+    	}else {
+    		System.out.println("not good to add, curr is " );
+    		textArea.appendText("Please select at least 1 topping.");
+    		
+    	}
+    	
+    	
+
+    }
+    
+    
     @FXML
     void actionShowOrder(ActionEvent event) {
 
-    }
-
-
-    
-    
-    
-
-    @FXML
-    void selectedDeluxe(ActionEvent event) {
-
-    	toppingsList.getItems().clear();
-    	toppingsList.setDisable(true);
-    	buttonAddTopping.setDisable(true);
-    	buttionRemoveTopping.setDisable(true);
-    	selectedToppingsList.getItems().clear();
-    	selectedToppingsList.getItems().addAll("Sausage", "Pepperoni", "Green Pepper", 
-    			"Onion", "Mushroom");// collection?
+    	//implement 
     	
-    }
-
-    @FXML
-    void selectedHawaiian(ActionEvent event) {
-    	toppingsList.getItems().clear();
-    	toppingsList.setDisable(true);
-    	buttonAddTopping.setDisable(true);
-    	buttionRemoveTopping.setDisable(true);
-    	selectedToppingsList.getItems().clear();
-    	selectedToppingsList.getItems().addAll("Ham", "Pineapple");
-    }
-    
-    @FXML
-    void selectBuildYourOwn(ActionEvent event) {
-    	// How to make this option default??
-    	toppingsList.getItems().clear();
-    	toppingsList.setDisable(false);
-    	buttonAddTopping.setDisable(false);
-    	selectedToppingsList.getItems().clear();
-
-    	toppingsList.getItems().addAll("Chicken", "Ham", "Mushroom", "Onion", "Pepperoni",
-    										"Pineapple", "Saussage","Beef", "Cheese", "Green Pepper");
-
-        
-    }
-    
-    
-    
-    
-    
-
-    @FXML
-    void selectLarge(ActionEvent event) {
-
-    }
-
-    @FXML
-    void selectMedium(ActionEvent event) {
-
-    }
-
-    @FXML
-    void selectSmall(ActionEvent event) {
-
     }
 
 
