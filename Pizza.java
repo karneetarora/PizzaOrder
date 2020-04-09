0eb82873bc9546eabf8394c669c51c99c520e2c8
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-04-09 17:00:07 -0400

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -1,17 +1,31 @@
+ /**
+ * Pizza Abstract Class
+ * Methods include - pizzaPrice(), toString()
+ * 
+ * @author Karneet Arora, Manel Bermad
+ */
+
 package application;
 
 import java.util.ArrayList;
 
-public abstract class Pizza {
-protected String style;
-protected String size;
-protected ArrayList<String> toppings;
-public Pizza(String style, String size, ArrayList<String> toppings) { }
-public Pizza(String style, String size) { }
-public abstract int pizzaPrice();
-public String toString() { 
-	//implement
-
-}
-
-}
+public abstract class Pizza {  
+	protected String style;  
+	protected String size;  
+	protected ArrayList<String> toppings;   
+	public Pizza(String style, String size, ArrayList<String> toppings) { 
+		this.style = style; 
+		this.size = size; 
+		this.toppings = toppings; 
+	}    
+	public Pizza(String style, String size) { }    
+	public abstract int pizzaPrice();   
+	
+	/**
+	* toString print the style, size, and toppings of the pizza 
+	* @return pizza details as a String
+	*/
+	public String toString() {  
+		return this.style + " " + this.size + " " + this.toppings; 
+	}        
+}  
