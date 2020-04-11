d3e4d1a13f317b77d1e800284a8c51d616d68938
Karneet Arora <44528134+karneetarora@users.noreply.github.com>
2020-04-11 12:27:53 -0400

    Add files via upload

---
+++ scene2.java
@@ -0,0 +1,35 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class scene2 {
+
+    @FXML
+    private Button buttonBack;
+
+    @FXML
+    private Button buttonClear;
+    
+    @FXML
+    private TextArea pizzaDisplay;
+    
+    @FXML
+    private TextArea numberOfPies;
+    
+    @FXML
+    private TextArea totalPrice;
+    
+    @FXML
+    void actionGoBack(ActionEvent event) {
+ 		
+    }
+    
+    @FXML
+    void actionClear(ActionEvent event) {
+ 		
+    }
+    
+}
